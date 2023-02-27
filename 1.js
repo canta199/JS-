04aa1b0efc6c1322be3e21aa6ff2e9719d9849ce
Maksim <canta.klayc@mail.ru>
2023-02-27 11:18:44 +0100

    delete feature

--- 1.js
+++ 1.js
@@ -5,3 +5,13 @@ myCity.popular = true;
 console.log(myCity);
 myCity.country = 'USA'
 console.log(myCity);
+// Добавление новых свойств в обьектах
+
+// Удаление свойств
+const myName = {
+  lastName: 'Pryshchyk',
+  firstName: 'Maksim',
+  country: 'Poland'
+}
+delete myName.lastName;
+console.log(myName);
